b3476e9af61ef2fc47ce22b78d39ae058384c386
María Zubeldia <mzubeldia09@gmail.com>
2021-12-20 17:51:10 +0100

    add html structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>jartura games</h1>
+      <header>
+        <h1>jartura games</h1>
+      </header>
+      <main>
+        <section className="section_solution">
+          <div>
+            imagen
+            <img></img>
+          </div>
+
+          <div>
+            <p>solucion</p>
+          </div>
+        </section>
+        <section className="section_form_game">
+          <form>
+            <input placeholder="chosen word"></input>
+
+            <button>submit</button>
+
+            <input disabled="disabled" placeholder="number of errors"></input>
+          </form>
+        </section>
+      </main>
+
+      <footer>
+      &copy; jartura games
+      logo
+      </footer>
     </div>
   );
 }
 
 export default App;
+/*
+    <header>
+        <h1>jartura games</h1>
+      </header>
+
+      <main>
+
+        <section className="section_solution">
+          <div>
+            <img>imagen</img>
+          </div>
+          <div></div>
+        </section>
+
+        <section className="section_form_game">
+          <form>
+            <input>letra elegida</input>
+            <button></button>
+          </form>
+        </section>
+
+      </main>
+*/
