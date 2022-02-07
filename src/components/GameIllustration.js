abb723734155fd0a761ccca7d13226c0b8d81406
María Zubeldia <mzubeldia09@gmail.com>
2022-02-07 20:00:29 +0100

    add comments about hangman svg

--- src/components/GameIllustration.js
+++ src/components/GameIllustration.js
@@ -14,9 +14,12 @@ const GameIllustration = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx="26" cy="51" r="16" fill="#C4C4C4" />
+        {/* rod bottom - horca pie */}
         <rect y="145" width="160" height="15" fill="#C4C4C4" />
         <rect x="150" y="6" width="10" height="139" fill="#C4C4C4" />
+
+        {/* rod top and rope - horca cabeza y cuerda */}
+ 
         <path d="M35 0H160V6H35V0Z" fill="#C4C4C4" />
         <line
           x1="36.8706"
@@ -26,6 +29,9 @@ const GameIllustration = () => {
           stroke="#C4C4C4"
         />
         <line x1="35.5" y1="4" x2="35.5" y2="33" stroke="#C4C4C4" />
+
+        {/* head and neck - cabeza y cuello */}
+        <circle cx="26" cy="51" r="16" fill="#C4C4C4" />
         <rect x="33" y="31" width="6" height="2" fill="#C4C4C4" />
         <rect x="33" y="33" width="7" height="2" fill="#C4C4C4" />
         <rect x="35" y="35" width="5" height="2" fill="#C4C4C4" />
@@ -34,10 +40,12 @@ const GameIllustration = () => {
           d="M33 69C33 70.1046 31.2091 71 29 71C26.7909 71 25 70.1046 25 69C25 67.8954 26.7909 67 29 67C31.2091 67 33 67.8954 33 69Z"
           fill="#C4C4C4"
         />
+        {/* core - cuerpo */}
         <path
           d="M19 77C19 73.6863 21.6863 71 25 71H33C36.3137 71 39 73.6863 39 77V106C39 109.314 36.3137 112 33 112H25C21.6863 112 19 109.314 19 106V77Z"
           fill="#C4C4C4"
         />
+        {/* arms - brazos */}
         <rect
           x="32.7711"
           y="83.0328"
@@ -56,6 +64,7 @@ const GameIllustration = () => {
           transform="rotate(32.777 20.2341 79.0361)"
           fill="#C4C4C4"
         />
+        {/* legs - piernas */}
         <rect
           x="30.8434"
           y="106.859"
