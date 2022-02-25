2ca6206142870c16cfd5782fd33d70f936e64715
Alba V <albavidalm@gmail.com>
2022-02-25 20:50:03 +0100

    Add an async await mode to get the API

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,24 @@
-const getApiData = () => {
-  return fetch("https://palabras-aleatorias-public-api.herokuapp.com/random")
-    .then((response) => response.json())
-    .then((response) => {
-      const result = response.body.Word;
+//Getting API with FETCH
 
-      return console.log(result);
-    });
+// const getApiData = () => {
+//   return fetch("https://palabras-aleatorias-public-api.herokuapp.com/random")
+//     .then((response) => response.json())
+//     .then((response) => {
+//       const result = response.body.Word;
+
+//       return console.log(result);
+//     });
+// };
+// export default getApiData;
+
+//Getting API with async await. To works it needs the line 9 at App.js (import the component). I think is for the state
+const getApiData = () => {
+  const getWord = async () => {
+    const url = "https://palabras-aleatorias-public-api.herokuapp.com/random";
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data.body.Word);
+  };
+  getWord();
 };
-export default getApiData;
+getApiData();
